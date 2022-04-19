e4ce46fe1c05442060c8a9d7bb1c7c5a1059230e
Florian Gi <86287152+Saphii@users.noreply.github.com>
2022-04-19 16:24:38 +0200

    Initialisation page Admin

---
+++ ebn_front/src/pages/DashboardAdmin.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/component/_DashboardAdmin.scss";
+
+const DashboardAdmin = () => {
+  return (
+    <div className="App">
+      <div className="AppGlass"></div>
+    </div>
+  );
+};
+
+export default DashboardAdmin;
