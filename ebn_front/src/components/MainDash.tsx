14bbd62954fbb6bcc55023d3dbdc8bbd32c08307
Saphii <86287152+Saphii@users.noreply.github.com>
2022-04-25 16:20:02 +0200

    Ajout Composants Ratio Déchets/Composte
    Voir maquette Admin

--- ebn_front/src/components/MainDash.tsx
+++ ebn_front/src/components/MainDash.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import '../styles/component/_MainDash.scss'
 import Cards from '../components/Cards'
 import StatCollectAdmin from "./StatCollectAdmin";
+import RatioWasteCompost from "./RatioWasteCompost";
 
 const MainDash = () => {
     return (
         <div className="MainDash">
             <h1>Tableau de bord</h1>
             <Cards/>
+            <div className="MainDash__bioStat">
             <StatCollectAdmin/>
+            <RatioWasteCompost/>
+        </div>
 
 
 
