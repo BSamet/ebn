478d746a0bb4b4b30682059a5e70651c956b4bdc
Bryanlaptop <bryan.chaine@gmail.com>
2022-04-26 15:59:43 +0200

    Bryan Feature : Page dashbord client header done

--- ebn_App/src/screens/Dashbord_Client/DashBordClient.tsx
+++ ebn_App/src/screens/Dashbord_Client/DashBordClient.tsx
@@ -1,12 +1,80 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  useWindowDimensions,
+  View,
+} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import Logo from '../../../assets/images/logo.png';
 
 const DashBordClient = () => {
+  const {height} = useWindowDimensions();
   return (
-    <View>
-      <Text> DASHBORD Client </Text>
-    </View>
+    <ScrollView>
+      <View style={styles.page}>
+        <View style={styles.header}>
+          <LinearGradient
+            colors={['#8AC997', '#0096f0']}
+            start={{
+              x: 0,
+              y: 1,
+            }}
+            end={{
+              x: 1,
+              y: 2,
+            }}
+            style={styles.box}>
+            <Image
+              source={Logo}
+              style={[styles.Logo, {height: height * 0.3}]}
+              resizeMode="contain"
+            />
+            <Text style={styles.topText}>Bonjour, Nom Client</Text>
+          </LinearGradient>
+        </View>
+
+        <View style={styles.body}>
+          <Text style={styles.titleText}>Historique de collecte</Text>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
+const styles = StyleSheet.create({
+  box: {
+    width: '100%',
+    height: 200,
+    borderBottomRightRadius: 25,
+    borderBottomLeftRadius: 25,
+  },
+  page: {
+    backgroundColor: 'white',
+  },
+  header: {},
+
+  topText: {
+    color: 'black',
+    fontSize: 20,
+    marginLeft: 20,
+  },
+
+  Logo: {
+    width: '70%',
+    height: 175,
+    maxHeight: 150,
+    marginLeft: 120,
+  },
+  body: {
+    paddingHorizontal: 10,
+    marginVertical: 100,
+  },
+  titleText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
 
 export default DashBordClient;
