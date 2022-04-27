060d942e1b6072c892e48785e0aed9728d94e1fd
Saphii <86287152+Saphii@users.noreply.github.com>
2022-04-27 10:16:26 +0200

    Ajout historique en dur /partie client

--- ebn_front/src/components/RightSideClient.tsx
+++ ebn_front/src/components/RightSideClient.tsx
@@ -3,8 +3,15 @@ import '../styles/component/_RightSideClient.scss'
 
 const RightSideClient = () => {
     return (
-        <div>
-            
+        <div className="historique">
+            <h2>Historique</h2>
+            <ul>
+                <li>Mardi 05/09/2022 - Bio déchets - 10kg</li>
+                <li>jeudi 07/09/2022 - Marc de café - 15kg</li>
+                <li>Mardi 12/09/2022 - Bio déchets - 10kg</li>
+                <li>Jeudi 14/09/2022 - Marc de café - 7kg</li>
+
+            </ul>
         </div>
     );
 };
