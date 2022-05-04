799f18e7c9da190d6f8a57247ef70827c276120c
kojo68 <62939146+kojo68@users.noreply.github.com>
2022-05-04 10:50:38 +0200

    popUp depuis scan

--- ebn_App/src/components/popUp.tsx
+++ ebn_App/src/components/popUp.tsx
@@ -5,78 +5,80 @@ import InformationClient from './informationClient';
 import axios from 'axios';
 import {URL_API} from '@env';
 
-const popUp = (props: {scaleValue: any}) => {
-  const scaleValue = props.scaleValue;
-  const [modalVisible, setModalVisible] = useState(false);
-  const [poids, setPoids] = useState(0);
-  const [commentaire, setCommentaire] = useState('');
-  let data = {
-    typeAction: 'récupération du saut',
-    date: '2022-05-01T18:57:30.295Z',
-    typeDeDechet: 'Biodéchets',
-    commentaire: commentaire,
-    poids: poids,
-    clientId: 1,
-    collecteurId: 1,
-    conteneurId: 3,
-  };
-  const submit = () => {
-    postPoids();
-  };
-  const postPoids = () => {
-    axios
-      .post(URL_API + 'historique', data)
-      .then(res => {})
-      .catch(function (error) {
-        console.log(error + ' sur post');
-      });
-  };
-  return (
-    <View style={popUpStyles.centeredView}>
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={scaleValue}
-        onRequestClose={() => {
-          Alert.alert('Modal has been closed.');
-          setModalVisible(!modalVisible);
-        }}>
+const popUp = (props: { scaleValue: any }) => {
+    const scaleValue = props.scaleValue;
+    const [modalVisible, setModalVisible] = useState(false);
+    const [poids, setPoids] = useState(0);
+    const [commentaire, setCommentaire] = useState('');
+    let data = {
+        typeAction: 'récupération du saut',
+        date: '2022-05-01T18:57:30.295Z',
+        typeDeDechet: 'Biodéchets',
+        commentaire: commentaire,
+        poids: poids,
+        clientId: 1,
+        collecteurId: 1,
+        conteneurId: 3,
+    };
+    const submit = () => {
+        postPoids();
+    };
+    const postPoids = () => {
+        axios
+            .post(URL_API + 'historique', data)
+            .then(res => {
+            })
+            .catch(function (error) {
+                console.log(error + ' sur post');
+            });
+    };
+    return (
         <View style={popUpStyles.centeredView}>
-          <View style={popUpStyles.modalView}>
-            <InformationClient> </InformationClient>
-            <Text style={popUpStyles.modalTitre}> Information du seau </Text>
-            <TextInput
-              style={popUpStyles.input}
-              keyboardType="numeric"
-              onChangeText={pesser => setPoids(pesser)}
-              placeholder="Entrer le poids"
-            />
-            <Text style={popUpStyles.modalText}>
-              Vous avez collecter {poids} kg
-            </Text>
-            <TextInput
-              style={popUpStyles.input}
-              onChangeText={com => setCommentaire(com)}
-              placeholder="Entrer le commentaire"
-            />
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={scaleValue}
+                onRequestClose={() => {
+                    Alert.alert('Modal has been closed.');
+                    setModalVisible(!modalVisible);
+                }}>
+                <View style={popUpStyles.centeredView}>
+                    <View style={popUpStyles.modalView}>
+                        <InformationClient> </InformationClient>
+                        <Text style={popUpStyles.modalTitre}> Information du seau </Text>
+                        <TextInput
+                            style={popUpStyles.input}
+                            keyboardType="numeric"
+                            onChangeText={peser => setPoids(peser)}
+                            placeholder="Entrer le poids"
+                        />
+                        <Text style={popUpStyles.modalText}>
+                            Vous avez collecter {poids} kg
+                        </Text>
+                        <TextInput
+                            style={popUpStyles.input}
+                            onChangeText={com => setCommentaire(com)}
+                            placeholder="Entrer le commentaire"
+                        />
+                        TODO
+                        <Pressable
+                            style={[popUpStyles.button, popUpStyles.buttonClose]}
+                            onPress={() => {
+                                setModalVisible(!modalVisible);
+                                submit();
+                            }}>
+                            <Text style={popUpStyles.textStyle}> Enregistrer </Text>
+                        </Pressable>
+                    </View>
+                </View>
+            </Modal>
             <Pressable
-              style={[popUpStyles.button, popUpStyles.buttonClose]}
-              onPress={() => {
-                setModalVisible(!modalVisible);
-                submit();
-              }}>
-              <Text style={popUpStyles.textStyle}> Enregistrer </Text>
+                style={[popUpStyles.button, popUpStyles.buttonOpen]}
+                onPress={() => setModalVisible(true)}>
+                <Text style={popUpStyles.textStyle}> pésage </Text>
             </Pressable>
-          </View>
         </View>
-      </Modal>
-      <Pressable
-        style={[popUpStyles.button, popUpStyles.buttonOpen]}
-        onPress={() => setModalVisible(true)}>
-        <Text style={popUpStyles.textStyle}> pésage </Text>
-      </Pressable>
-    </View>
-  );
+    );
 };
 
 export default popUp;
