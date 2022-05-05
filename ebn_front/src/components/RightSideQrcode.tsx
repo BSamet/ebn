299c5a7bc75a999101f3b1117fa311d685762cb9
Maknassi Kamal <kamal.maknassi@uha.fr>
2022-05-05 14:50:24 +0200

    Id conteneurs modifie le QRCODE qd on appue sur un conteneur de la liste

--- ebn_front/src/components/RightSideQrcode.tsx
+++ ebn_front/src/components/RightSideQrcode.tsx
@@ -4,54 +4,55 @@ import "../styles/component/_QrCodeGen.scss"
 const icon = require("../assets/cycle.png")
 
 interface idConteneurInterface {
-    selectConteneurId : string;
+    selectConteneurId: string;
 }
 
 
-const RightSideQrcode = ({selectConteneurId}: idConteneurInterface) => {
+const RightSideQrcode = ({ selectConteneurId }: idConteneurInterface) => {
     const qrRef = React.useRef() as React.MutableRefObject<HTMLInputElement>;
-    const [text , setText] = React.useState("");
-    
+    const [text, setText] = React.useState("");
+
     const printQRCode = (evt: React.FormEvent) => {
         evt.preventDefault();
         setText("");
     };
-    console.log(text);
-    
+
     const qrCode = (
-    <QRCode
-        size={250}
-        value={selectConteneurId}
-        bgColor="white"
-        fgColor="black"
-        level="H"
-        imageSettings={{
-            src: icon,
-            excavate: true,
-            width: 250 * 0.1,
-            height: 250 * 0.1
-        }}
+        <QRCode
+            size={250}
+            value={"" + selectConteneurId}
+            bgColor="white"
+            fgColor="black"
+            level="H"
+            imageSettings={{
+                src: icon,
+                excavate: true,
+                width: 250 * 0.1,
+                height: 250 * 0.1
+            }}
         />
     )
-    console.log(qrCode);
-    
+
+    if(selectConteneurId !== "") {
         return (
             <div className="qr-container">
-            <form onSubmit={printQRCode} className="qr-container__form">
-                <input
-                    type="text"
-                    value={text}
-                    onChange={(e) => setText(e.target.value)}
-                    placeholder="12345678"
-                />
-
-                <button type="submit">Imprimer le QR Code</button>
-            </form>
-
-            <div className="qr-container__qr-code" ref={qrRef}>
-                {qrCode}
+                {selectConteneurId}
+                <form onSubmit={printQRCode} className="qr-container__form">
+    
+                    <button type="submit">Imprimer le QR Code</button>
+                </form>
+    
+                <div className="qr-container__qr-code" ref={qrRef}>
+                    {qrCode}
                 </div>
-        </div>
-    );
+            </div>
+        );
+    } else {
+        return (
+            <div className="qr-container">
+                <h1>Veuillez séléctionnez un conteneur</h1>
+            </div>
+        )
     }
-    export default RightSideQrcode
+}
+export default RightSideQrcode
