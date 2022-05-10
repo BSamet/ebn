9c47d3810fb1642f08124d39b7e098ddb3ea1b9c
kojo68 <62939146+kojo68@users.noreply.github.com>
2022-05-10 12:03:15 +0200

    commit post  fonctionnel

--- ebn_App/src/components/popUp.tsx
+++ ebn_App/src/components/popUp.tsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {Modal, Pressable, Text, TextInput, View} from 'react-native';
+import {Picker} from '@react-native-picker/picker';
 import popUpStyles from '../styles/popUpStyles';
 import axios from 'axios';
 import {HOST_BACK} from "../../environment/environment";
@@ -20,6 +21,7 @@ interface conteneurInterface {
 const popUp = (props: any) => {
     const [info, setInfo] = useState<conteneurInterface>();
     const scanValue = props.data;
+    const [selectedValue, setSelectedValue] = useState();
     const [conteneur, setConteneur] = useState();
     const [modalVisible, setModalVisible] = useState(true);
     const [poids, setPoids] = useState(0);
@@ -73,7 +75,13 @@ const popUp = (props: any) => {
                     <View style={popUpStyles.modalView}>
                         <Text style={popUpStyles.modalTitre}> Information du seau </Text>
                         <Text> </Text>
-
+                        <Picker
+                            selectedValue={selectedValue}
+                            style={{height: 50, width: 150}}
+                            onValueChange={(itemValue) => setSelectedValue(itemValue)}>
+                            <Picker.Item label="Récupération du seau" value="Récupération du seau"/>
+                            <Picker.Item label="dépot du seau" value="dépot du seau"/>
+                        </Picker>
                         <TextInput
                             style={popUpStyles.input}
                             keyboardType="numeric"
