10cc30818bf4d7a4950b7243f64d08beb2da561f
Bryanlaptop <bryan.chaine@gmail.com>
2022-05-10 17:01:07 +0200

    dashbord client : affichage des tournées , modélisation de la modal en cours

--- ebn_App/src/screens/Dashbord_Client/DashBordClient.tsx
+++ ebn_App/src/screens/Dashbord_Client/DashBordClient.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import axios from 'axios';
+import moment from 'moment';
+import React, {useEffect, useState} from 'react';
 import {
   Image,
+  Modal,
+  Pressable,
   ScrollView,
   StyleSheet,
   Text,
@@ -9,54 +13,84 @@ import {
 } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import Logo from '../../../assets/images/logo.png';
-import PopUp from '../../components/popUp';
+import {HOST_BACK} from '../../../environment/environment';
 
 export interface dashboardClient {
-  Day: string;
-  Month: string;
-  Year: string;
+  id: number;
+  date: string;
+  isCollected: boolean;
+  commentaire: string;
+
+  collecteur: {
+    numeroCollecteur: number;
+    numeroVelo: number;
+    utilisateur: {
+      id: number;
+      role: string;
+      nom: string;
+      prenom: string;
+      mail: string;
+      telephone: string;
+    };
+  };
+}
+export interface HistoriqueClient {
+  historique: {
+    id: number;
+    typeAction: string;
+    date: number;
+    typeDeDechet: string;
+    commentaire: string;
+    poids: number;
+  };
+}
+export interface ShowClient {
+  client: {
+    id: number;
+    siret: number;
+    nomCommercial: string;
+    adresse: string;
+    utilisateur: {
+      id: number;
+      role: string;
+      utilisateur: string;
+      password: string;
+      nom: string;
+      prenom: string;
+      mail: string;
+      telephone: string;
+    };
+  };
 }
 
 // TODO rendre la list cliquable OnPress() et faire intervenir les données
 const DashBordClient = () => {
-  const list = [
-    {
-      Day: 'Vendredi 13',
-      Month: 'Novembre',
-      Year: '2022',
-      weight: 67,
-    },
-    {
-      Day: 'Samedi 14',
-      Month: 'Novembre',
-      Year: '2022',
-      weight: 98,
-    },
-    {
-      Day: 'Lundi 16',
-      Month: 'Novembre',
-      Year: '2022',
-      weight: 34,
-    },
-    {
-      Day: 'Jeudi 19',
-      Month: 'Novembre',
-      Year: '2022',
-      weight: 23,
-    },
-    {
-      Day: 'Vendredi 20',
-      Month: 'Novembre',
-      Year: '2022',
-      weight: 12,
-    },
-    {
-      Day: 'Samedi 21',
-      Month: 'Novembre',
-      Year: '2022',
-      weight: 35,
-    },
-  ];
+  const [fetchOnce, setFetchOnce] = useState(true);
+  const [tourner, setTouner] = useState<dashboardClient[]>();
+  const [myclient, setMyClient] = useState<ShowClient>();
+  const [historiqueModal, setHistoriqueModal] = useState<HistoriqueClient>();
+  const [modalOpen, setModalOpen] = useState(false);
+
+  useEffect(() => {
+    if (fetchOnce) {
+      axios.get(HOST_BACK + '/etape/client/1').then(res => {
+        // appel de l'api
+
+        setMyClient(res.data[0]);
+
+        setTouner(res.data); // recuperer les infos du collecteur sans map
+
+        // on cherche une seul fois
+        setFetchOnce(false);
+      });
+    }
+  }, [tourner, myclient, fetchOnce]);
+
+  const showModal = (Historique: any) => {
+    setModalOpen(true);
+    setHistoriqueModal(Historique);
+  };
+
   const {height} = useWindowDimensions();
   return (
     <ScrollView>
@@ -78,16 +112,40 @@ const DashBordClient = () => {
               style={[styles.Logo, {height: height * 0.3}]}
               resizeMode="contain"
             />
-            <Text style={styles.topText}>Bonjour, Nom Client</Text>
+            <Text style={styles.topText}>
+              Bonjour, {myclient?.client.nomCommercial}
+            </Text>
           </LinearGradient>
         </View>
         <Text style={styles.titleText}>Historique de collecte</Text>
-        {list.map((item, index) => (
+
+        <Modal animationType="slide" transparent={true} visible={modalOpen}>
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalText}>
+                Nom : {historiqueModal?.historique.commentaire}
+              </Text>
+
+              <Pressable
+                style={[styles.buttonModal, styles.buttonClose]}
+                onPress={() => setModalOpen(!modalOpen)}>
+                <Text style={styles.textStyle}>Fermer</Text>
+              </Pressable>
+            </View>
+          </View>
+        </Modal>
+        {tourner?.map((item, index) => (
           <View style={styles.body} key={index}>
-            <Text style={styles.date}>
-              {item.Day} {item.Month} {item.Year}
-            </Text>
-            <Text style={styles.poids}>poids collecté : {item.weight} KG </Text>
+            <Pressable onPress={() => showModal(item)}>
+              <Text style={styles.date}>
+                {item.collecteur.utilisateur.nom}{' '}
+                {item.collecteur.utilisateur.prenom}
+              </Text>
+              <Text style={styles.poids}>
+                Heure approximative de votre collecte {'  '}
+                {moment(item.date).format('DD.MM.YYYY  à HH[h] mm')}
+              </Text>
+            </Pressable>
           </View>
         ))}
       </View>
@@ -96,6 +154,47 @@ const DashBordClient = () => {
 };
 
 const styles = StyleSheet.create({
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  buttonClose: {
+    backgroundColor: 'transparent',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+    fontFamily: 'Confortaa-Bold',
+    color: '#8AC997',
+    fontSize: 20,
+  },
+  buttonModal: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
   box: {
     width: '100%',
     height: 200,
