482eea6a4f936b838308aa0d406f18f5c9fb9050
Saphii <86287152+Saphii@users.noreply.github.com>
2022-05-10 14:55:19 +0200

    Ajout du back pour le tri de l'historique

--- ebn_back/src/historique/historique.controller.ts
+++ ebn_back/src/historique/historique.controller.ts
@@ -1,40 +1,68 @@
-import {Body, Controller, Delete, Get, Param, Patch, Post} from '@nestjs/common';
-import {HistoriqueService} from './historique.service';
-import {CreateHistoriqueDto} from './dto/create-historique.dto';
-import {UpdateHistoriqueDto} from './dto/update-historique.dto';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { HistoriqueService } from './historique.service';
+import { CreateHistoriqueDto } from './dto/create-historique.dto';
+import { UpdateHistoriqueDto } from './dto/update-historique.dto';
 
 @Controller('historique')
 export class HistoriqueController {
-    constructor(private readonly historiqueService: HistoriqueService) {
-    }
+  constructor(private readonly historiqueService: HistoriqueService) {}
 
-    @Post()
-    create(@Body() createHistoriqueDto: CreateHistoriqueDto) {
-        return this.historiqueService.create(createHistoriqueDto);
-    }
+  @Post()
+  create(@Body() createHistoriqueDto: CreateHistoriqueDto) {
+    return this.historiqueService.create(createHistoriqueDto);
+  }
 
-    @Get()
-    findAll() {
-        return this.historiqueService.findAll();
-    }
+  @Get()
+  findAll() {
+    return this.historiqueService.findAll();
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        return this.historiqueService.findOne(+id);
-    }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.historiqueService.findOne(+id);
+  }
 
-    @Get('customer/:id')
-    findByClient(@Param('id') id: string) {
-        return this.historiqueService.findByClient(+id);
-    }
+  @Get('customer/:id')
+  findByClient(@Param('id') id: string) {
+    return this.historiqueService.findByClient(+id);
+  }
 
-    @Patch(':id')
-    update(@Param('id') id: string, @Body() updateHistoriqueDto: UpdateHistoriqueDto) {
-        return this.historiqueService.update(+id, updateHistoriqueDto);
-    }
+  @Get('/all/:pages')
+  findAllHistoriquesPagination(
+    @Param('pages') pages: number,
+    @Query('take') take: number,
+    @Query('orderBy') orderBy: string,
+  ) {
+    const takeForBuilder = take || 10;
+    const pagesForBuilder = pages || 1;
+    const skipForBuilder = takeForBuilder * (pagesForBuilder - 1);
 
-    @Delete(':id')
-    remove(@Param('id') id: string) {
-        return this.historiqueService.remove(+id);
-    }
+    return this.historiqueService.findAllHistoriquesPagination(
+      takeForBuilder,
+      skipForBuilder,
+      orderBy,
+    );
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateHistoriqueDto: UpdateHistoriqueDto,
+  ) {
+    return this.historiqueService.update(+id, updateHistoriqueDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.historiqueService.remove(+id);
+  }
 }
--- ebn_front/src/components/DashboardAdminHistory.tsx
+++ ebn_front/src/components/DashboardAdminHistory.tsx
@@ -1,32 +1,118 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import {HOST_BACK} from "../environment/environment";
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
+import ListItemButton from "@mui/material/ListItemButton";
+import moment from "moment";
+import {useParams} from "react-router-dom";
+import Stack from "@mui/material/Stack";
+import Pagination from "@mui/material/Pagination";
+
+
+interface historyCustomerInterface{
+
+    typeAction:string;
+    commentaire:string;
+    date:number;
+    typeDeDechet:string;
+    poids:number;
+
+    client:{
+        nomCommercial:string;
+        utilisateur:{
+            nom:string;
+            prenom:string;
+        }
+    }
+
+
+
+}
+
+
+
+
+
+
 
 const DashboardAdminHistory = () => {
-    const [history, setHistory] = useState<any[]>([]);
+
+
+    const [DashboardAdminHistory, setDashboardAdminHistory] = useState<historyCustomerInterface[]>();
+    const [fetchOnce, setFetchOnce] = useState(true);
+    const formatDate = moment().format('DD-MM-YYYY')
+    const [totalPages, setTotalPages] = React.useState('')
+    const [page, setPage] = React.useState(1);
+
+    const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+        setPage(value);
+        axios.get(HOST_BACK + '/historique/all/' + value + '?take=5').then(res => {
+            setDashboardAdminHistory(res.data.historiques)
+            console.log(res.data)
+        });
+    };
+
+
 
     useEffect(() => {
-        axios
-            .get(HOST_BACK +'/historique')
-            .then(res => {
-                setHistory(res.data);
-            })
-            .catch(function (error) {
-                console.log(error);
+        if (fetchOnce) {
+            axios.get(HOST_BACK + '/historique/all/' + page + '?take=5&orderBy=date').then(res => {
+                setDashboardAdminHistory(res.data.historiques)
+                // appel de l'api
+                setFetchOnce(false);
+                setTotalPages(res.data.totalPages)
             });
-    }, []);
+        }
+    }, [DashboardAdminHistory, fetchOnce]);
+
+
     return (
-        <div>
-            {history.map((data) => (
-                <div>
-                    <h1>{data.typeAction}</h1>
-                    <p>{data.client.utilisateur.nom} {data.client.utilisateur.prenom}</p>
-                    <p>{data.date}</p>
-                    <p>{data.commentaire}</p>
-                </div>
-            ))}
+
+        <div className='conteneurs'>
+            <h1>LISTE DES HISTORIQUES</h1>
+        <div className='liste'>
+            <Box sx={{width: '80%', bgcolor: 'background.paper'}}>
+
+                <List component="nav" aria-label="Liste des conteneurs">
+                    <ListItem className='listItemHeader'>
+                        <ListItemText className='listHeader' primary="Nom Commercial"/>
+                        <ListItemText className='listHeader' primary="Action"/>
+                        <ListItemText className='listHeader' primary="Type de déchet"/>
+                        <ListItemText className='listHeader' primary="Date"/>
+                        <ListItemText className='listHeader' primary="Commentaire"/>
+                    </ListItem>
+                    <ListItem className='listItemHeader'>
+                        <ListItemText className='listHeader' primary=" "/>
+                    </ListItem>
+                    <Divider/>
+
+                    {DashboardAdminHistory?.map((list,index) =>
+                        <ListItem className="listItemBody">
+                        <ListItemText className='listItem' primary={list.client.nomCommercial}/>
+                            <ListItemText className='listItem' primary={list.typeAction}/>
+                            <ListItemText className='listItem' primary={list.typeDeDechet}/>
+                            <ListItemText className='listItem' primary={moment(list.date).format('DD.MM.YYYY à HH [h] mm')}/>
+                            <ListItemText className='listItem' primary={list.commentaire}/>
+
+                        </ListItem>
+
+                    )}
+                </List>
+            </Box>
         </div>
-    );
-};
+
+            <div className='pagination'>
+                <Stack spacing={2}>
+                    <Pagination count={totalPages} color="primary" page={page} onChange={handleChange}/>
+                </Stack>
+            </div>
+
+        </div>
+    )
+}
 
 export default DashboardAdminHistory;
