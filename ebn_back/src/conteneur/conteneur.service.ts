dbf6c425ee7d1576e77479a0bb99a67687e84b82
kojo68 <62939146+kojo68@users.noreply.github.com>
2022-05-12 14:55:55 +0200

    commit module manquant

--- ebn_back/src/conteneur/conteneur.service.ts
+++ ebn_back/src/conteneur/conteneur.service.ts
@@ -1,73 +1,77 @@
-import { Injectable } from '@nestjs/common';
-import { CreateConteneurDto } from './dto/create-conteneur.dto';
-import { UpdateConteneurDto } from './dto/update-conteneur.dto';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { Conteneur } from './entities/conteneur.entity';
-import { Client } from '../client/entities/client.entity';
-import { TypeDechet } from '../type-dechets/entities/type-dechet.entity';
+import {Injectable} from '@nestjs/common';
+import {CreateConteneurDto} from './dto/create-conteneur.dto';
+import {UpdateConteneurDto} from './dto/update-conteneur.dto';
+import {InjectRepository} from '@nestjs/typeorm';
+import {Repository} from 'typeorm';
+import {Conteneur} from './entities/conteneur.entity';
+import {Client} from '../client/entities/client.entity';
+import {TypeDechet} from '../type-dechets/entities/type-dechet.entity';
 
 @Injectable()
 export class ConteneurService {
-  constructor(
-    @InjectRepository(Conteneur)
-    private readonly conteneurRepository: Repository<Conteneur>,
-  ) {}
-
-  create(createConteneurDto: CreateConteneurDto) {
-    const conteneur = new Conteneur();
-    conteneur.capaciteMax = createConteneurDto.capaciteMax;
-    conteneur.isAvailable = createConteneurDto.isAvailable;
-    conteneur.typeDechet = Object.assign(new TypeDechet(), {
-      id: createConteneurDto.typeDechetId,
-    });
-    if (createConteneurDto.clientId) {
-      conteneur.client = Object.assign(new Client(), {
-        id: createConteneurDto.clientId,
-      });
+    constructor(
+        @InjectRepository(Conteneur)
+        private readonly conteneurRepository: Repository<Conteneur>,
+    ) {
     }
-    return this.conteneurRepository.save(conteneur);
-  }
 
-  findAll() {
-    return this.conteneurRepository.find();
-  }
+    create(createConteneurDto: CreateConteneurDto) {
+        const conteneur = new Conteneur();
+        conteneur.capaciteMax = createConteneurDto.capaciteMax;
+        conteneur.isAvailable = createConteneurDto.isAvailable;
+        conteneur.typeDechet = Object.assign(new TypeDechet(), {
+            id: createConteneurDto.typeDechetId,
+        });
+        if (createConteneurDto.clientId) {
+            conteneur.client = Object.assign(new Client(), {
+                id: createConteneurDto.clientId,
+            });
+        }
+        return this.conteneurRepository.save(conteneur);
+    }
 
-  findOne(id: number) {
-    return this.conteneurRepository.findOne(id);
-  }
+    findAll() {
+        return this.conteneurRepository.find();
+    }
 
-  async findAllConteneurPagination(take: number, skip: number) {
-    const countedConteneur = await this.conteneurRepository.count();
+    findOne(id: number) {
+        return this.conteneurRepository.findOne(id);
+    }
 
-    const totalPages = Math.ceil(countedConteneur / take);
+    async findAllConteneurPagination(take: number, skip: number) {
+        const countedConteneur = await this.conteneurRepository.count();
 
-    const allConteneur = await this.conteneurRepository
-      .createQueryBuilder('conteneur')
-      .leftJoinAndSelect('conteneur.client', 'client')
-      .leftJoinAndSelect('client.utilisateur', 'utilisateur')
-      .leftJoinAndSelect('conteneur.typeDechet', 'typeDechet')
-      .select('conteneur')
-      .addSelect('client.nomCommercial')
-      .addSelect('utilisateur.nom')
-      .addSelect('utilisateur.prenom')
-      .addSelect('typeDechet.typeDechets')
-      .take(take)
-      .skip(skip)
-      .getMany();
+        const totalPages = Math.ceil(countedConteneur / take);
 
-    return {
-      totalPages: totalPages,
-      totalConteneur: countedConteneur,
-      conteneurs: allConteneur,
-    };
-  }
+        const allConteneur = await this.conteneurRepository
+            .createQueryBuilder('conteneur')
+            .leftJoinAndSelect('conteneur.client', 'client')
+            .leftJoinAndSelect('client.utilisateur', 'utilisateur')
+            .leftJoinAndSelect('conteneur.typeDechet', 'typeDechet')
+            .select('conteneur')
+            .addSelect('client.nomCommercial')
+            .addSelect('utilisateur.nom')
+            .addSelect('utilisateur.prenom')
+            .addSelect('typeDechet.typeDechets')
+            .take(take)
+            .skip(skip)
+            .getMany();
 
-  update(id: number, updateConteneurDto: UpdateConteneurDto) {
-    return this.conteneurRepository.update(id, updateConteneurDto);
-  }
+        return {
+            totalPages: totalPages,
+            totalConteneur: countedConteneur,
+            conteneurs: allConteneur,
+        };
+    }
 
-  remove(id: number) {
-    return this.conteneurRepository.delete(id);
-  }
+    findOneWithAllInfos(id: number) {
+    }
+
+    update(id: number, updateConteneurDto: UpdateConteneurDto) {
+        return this.conteneurRepository.update(id, updateConteneurDto);
+    }
+
+    remove(id: number) {
+        return this.conteneurRepository.delete(id);
+    }
 }
