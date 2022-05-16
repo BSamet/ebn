3a957305095c67acf0ccf7456e795920421b43f6
samet <samet.sb.68@gmail.com>
2022-05-16 14:30:36 +0200

    Samet : Back - Update SQL File for database

--- ebn_back/src/app.module.ts
+++ ebn_back/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { getConnectionOptions } from 'typeorm';
 import { ClientModule } from './client/client.module';
 import { CollecteurModule } from './collecteur/collecteur.module';
 import { RamassagePonctuelModule } from './ramassag-ponctuel/ramassagePonctuel.module';
@@ -24,10 +23,10 @@ import { ConfigModule } from '@nestjs/config';
       username: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWORD,
       database: process.env.MYSQL_DATABASE,
-      entities: ["dist/**/*.entity{.ts,.js}"],
+      entities: ['dist/**/*.entity{.ts,.js}'],
       synchronize: true,
       logging: true,
-      ssl: false
+      ssl: false,
     }),
     ConfigModule.forRoot({ isGlobal: true }),
     ClientModule,
