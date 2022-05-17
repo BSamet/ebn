8e80ec55c0fe193ca52462109dfc195e342f7bea
samet <samet.sb.68@gmail.com>
2022-05-17 16:30:23 +0200

    Samet : Front - Désintégration totale des bouton section admin et section client

--- ebn_front/src/pages/Accueil.tsx
+++ ebn_front/src/pages/Accueil.tsx
@@ -2,92 +2,48 @@ import Header from "../components/Header";
 import MainPoste from "../components/MainPoste";
 import logo from "../assets/logo.png";
 import Footer from "../components/Footer";
-import Button from "@mui/material/Button";
-import axios from 'axios';
-import {HOST_BACK} from "../environment/environment";
-import React, {useEffect} from "react";
-import {useNavigate} from 'react-router-dom';
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Accueil = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        if (sessionStorage.getItem("role") == "Client") {
-            navigate("/client");
-        } else if (sessionStorage.getItem("role") == "Admin") {
-            navigate("/admin");
-        } else {
-            navigate("/");
-        }
-    }, [])
-
-    function setSessionStorage(id: number, role: string) {
-        if (role == "Client") {
-            axios.get(HOST_BACK + `/client/` + id)
-                .then(res => {
-                    sessionStorage.setItem("id", res.data.id);
-                    sessionStorage.setItem("role", res.data.utilisateur.role);
-                    sessionStorage.setItem("name", res.data.utilisateur.prenom);
-                    sessionStorage.setItem("lastname", res.data.utilisateur.name);
-                    sessionStorage.setItem("commercialName", res.data.nomCommercial);
-                    setTimeout(() => {
-                        navigate("/client")
-                    }, 100);
-                })
-        } else {
-            axios.get(HOST_BACK + `/utilisateurs/` + id)
-                .then(res => {
-                    sessionStorage.setItem("id", res.data.id);
-                    sessionStorage.setItem("role", res.data.role);
-                    sessionStorage.setItem("name", res.data.prenom);
-                    sessionStorage.setItem("lastname", res.data.name);
-                    setTimeout(() => {
-                        navigate("/admin")
-                    }, 100);
-                })
-        }
+  useEffect(() => {
+    if (sessionStorage.getItem("role") == "Client") {
+      navigate("/client");
+    } else if (sessionStorage.getItem("role") == "Admin") {
+      navigate("/admin");
+    } else {
+      navigate("/");
     }
+  }, []);
 
-    return (
-        <>
-            <div className="logo">
-                <img src={logo} alt="EBNlogo"/>
-            </div>
-            <div className="home">
-                <Header sections={[]} title={""}/>
-            </div>
+  return (
+    <>
+      <div className="logo">
+        <img src={logo} alt="EBNlogo" />
+      </div>
+      <div className="home">
+        <Header sections={[]} title={""} />
+      </div>
 
-            <div className="mainPost">
-                <MainPoste
-                    post={{
-                        description: "Bienvenue sur le site EBN",
-                        image:
-                            "https://cdn.radiofrance.fr/s3/cruiser-production/2021/08/129500c2-4aab-4f85-9e26-77817b63c827/1136_ecologie-rechauffement-climatique-changement.jpg",
-                        imageText: "test",
-                        linkText: "",
-                        title: "Venez nous découvrir",
-                    }}
-                />
-            </div>
-
-            <Button variant="contained" color="success" onClick={() =>
-                setSessionStorage(1, "Admin")
-            }>
-                Section Admin
-            </Button>
-            <Button variant="contained" color="success" onClick={() =>
-                setSessionStorage(1, "Client")
-            }>
-                Section Client
-            </Button>
-
-
-            <div className="footer">
-                <Footer description={"Maquette pour EBN"} title={"Bienvenue"}/>
-            </div>
-
-        </>
-    );
+      <div className="mainPost">
+        <MainPoste
+          post={{
+            description: "Bienvenue sur le site EBN",
+            image:
+              "https://cdn.radiofrance.fr/s3/cruiser-production/2021/08/129500c2-4aab-4f85-9e26-77817b63c827/1136_ecologie-rechauffement-climatique-changement.jpg",
+            imageText: "test",
+            linkText: "",
+            title: "Venez nous découvrir",
+          }}
+        />
+      </div>
+      <div className="footer">
+        <Footer description={"EBN"} title={"Bienvenue"} />
+      </div>
+    </>
+  );
 };
 
 export default Accueil;
--- ebn_front/src/pages/Connection.tsx
+++ ebn_front/src/pages/Connection.tsx
@@ -4,8 +4,6 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
@@ -136,11 +134,6 @@ export default function SignInSide() {
                 autoComplete="current-password"
                 onChange={(e) => setPassword(e.target.value)}
               />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Se souvenir de moi"
-              />
-
               <Button
                 type="submit"
                 fullWidth
@@ -151,11 +144,6 @@ export default function SignInSide() {
                 connexion
               </Button>
               <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Mot de passe oublié ?
-                  </Link>
-                </Grid>
                 <Grid item>
                   <Link href="/Inscription" variant="body2">
                     {"Pas encore chez nous ? Inscrivez vous !"}
