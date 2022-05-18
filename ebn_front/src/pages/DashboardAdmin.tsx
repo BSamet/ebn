f0bb1fa36cdd8e6918f1df92fc87ef5b0d5d2af9
samet <samet.sb.68@gmail.com>
2022-05-18 16:32:55 +0200

    Samet : Front - Bogue redirection fixed, I promise is the last commit in develop

--- ebn_front/src/pages/DashboardAdmin.tsx
+++ ebn_front/src/pages/DashboardAdmin.tsx
@@ -8,18 +8,21 @@ const DashboardAdmin = () => {
   const navigate = useNavigate();
 
   // séléction du composant central
-  const [selectNav, setSelectNav] = useState('');
+  const [selectNav, setSelectNav] = useState("");
   // séléction du composant de droite
-  const [selectRight, setSelectRight] = useState('');
+  const [selectRight, setSelectRight] = useState("");
   // passage de l'ID du conteneur de la liste centrale au QRcode situé à droite
-  const [selectConteneurId, setSelectConteneurId] = useState('');
+  const [selectConteneurId, setSelectConteneurId] = useState("");
   //passage de l'ID Client de la liste centrale vers les formulaires
-  const [selectClientId, setSelectClientId] = useState('');
+  const [selectClientId, setSelectClientId] = useState("");
   //passage de l'ID Collecteur de la liste centrale vers les formulaires
-  const [selectCollecteurId, setSelectCollecteurId] = useState('')
+  const [selectCollecteurId, setSelectCollecteurId] = useState("");
   useEffect(() => {
     const sessionExp: any = sessionStorage.getItem("token_exp");
     const role: any = sessionStorage.getItem("role");
+    if (!sessionStorage.getItem("role")) {
+      navigate("/");
+    }
     if (role !== "Admin") {
       navigate("/client");
     }
@@ -35,13 +38,20 @@ const DashboardAdmin = () => {
     <div className="App">
       <div className="AppGlass">
         <Sidebar setSelectNav={setSelectNav} setSelectRight={setSelectRight} />
-        <ViewAdminDash 
-          setSelectCollecteurId={undefined} selectNav={selectNav}
-          selectConteneurId={selectConteneurId} setSelectConteneurId={setSelectConteneurId}
-          setSelectClientId={setSelectClientId} selectClientId={selectClientId}
-          {...setSelectCollecteurId} selectCollecteurId={selectCollecteurId}          />
-        <ViewAdminRightSide selectRight={selectRight} selectConteneurId={selectConteneurId} />
-
+        <ViewAdminDash
+          setSelectCollecteurId={undefined}
+          selectNav={selectNav}
+          selectConteneurId={selectConteneurId}
+          setSelectConteneurId={setSelectConteneurId}
+          setSelectClientId={setSelectClientId}
+          selectClientId={selectClientId}
+          {...setSelectCollecteurId}
+          selectCollecteurId={selectCollecteurId}
+        />
+        <ViewAdminRightSide
+          selectRight={selectRight}
+          selectConteneurId={selectConteneurId}
+        />
       </div>
     </div>
   );
