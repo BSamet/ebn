0838662f921873662c406a02b2785b8de33275e0
ilies.hartout <ilyeshartout@gmail.com>
2022-06-10 15:54:09 +0200

    récuperation des etapes

--- ebn_front/src/components/DashboardHistoryCollecteur.tsx
+++ ebn_front/src/components/DashboardHistoryCollecteur.tsx
@@ -2,8 +2,10 @@ import "react-datepicker/dist/react-datepicker.css";
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import {HOST_BACK} from "../environment/environment";
-import { MenuItem } from "@mui/material";
+import { ListItem, ListItemText, MenuItem } from "@mui/material";
 import {FormControl, InputLabel, Select} from "@mui/material";
+import { listenerCount } from "process";
+import moment from "moment";
 
 
 
@@ -21,6 +23,7 @@ interface Collecteur {
 
 interface InformationEtape{
     date: Date;
+    clientid: number;
     client:{
         nomCommercial: string;
         nom: string;
@@ -31,20 +34,21 @@ interface InformationEtape{
 
 
 export function DashboardHistoryCollecteur () {
+    const [DashboardHistoryCollecteur, setDashboardHistoryCollecteur] = useState<Collecteur[]>();
     const [Collecteur, setCollecteur] = useState<Collecteur[]>();
     const [InformationEtape, setInformationEtape] = useState<InformationEtape[]>();
     const [fetchOnce, setFetchOnce] = useState(true);
     const [fetchCollectorOnce, setFetchCollectorOnce] = useState(true);
+    const [id, setid] = useState('');
 
 
 
     useEffect(() => {
-        if (fetchOnce) { axios.get(HOST_BACK + '/etape/collecteur/1', {
+        if (fetchOnce) { axios.get(HOST_BACK + '/etape', {
             headers: {
                 "Authorization": `Bearer ${sessionStorage.getItem('token')}`
             }}).then(res => {
             setInformationEtape(res.data)
-            console.log("etape:" + InformationEtape)
             setFetchOnce(false)
             
             })
@@ -56,16 +60,30 @@ export function DashboardHistoryCollecteur () {
         axios.get(HOST_BACK + '/collecteur',{
             headers: {
                 "Authorization": `Bearer ${sessionStorage.getItem('token')}`
-            }
-        }).then(res => {
-            console.log(res.data)
+            
+            }}).then(res => {
             setCollecteur(res.data)
-            console.log("collecteur:" + Collecteur)
+            setDashboardHistoryCollecteur(res.data.historiques)
+
             setFetchCollectorOnce(false)
+
 
             })
         }
     }, [fetchCollectorOnce, Collecteur])
+    
+    function validateFilter(){
+        axios.get(HOST_BACK + '/collecteur',{
+            headers: {
+                "Authorization": `Bearer ${sessionStorage.getItem('token')}`
+            
+            }}).then(res => {
+            setCollecteur(res.data)
+            setDashboardHistoryCollecteur(res.data.historiques)
+
+            setFetchCollectorOnce(false)
+        })
+    } 
 
     return(
     <div>
@@ -73,30 +91,44 @@ export function DashboardHistoryCollecteur () {
 
             <div>
         <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">Age</InputLabel>
+            <InputLabel id="demo-simple-select-label">collecteur</InputLabel>
             <Select
                 labelId="demo-simple-select-label"
                 id="demo-simple-select"
-                // value={agendaCollecteur}
-                label="Age"
-                // onChange={agendaCollecteur}
-            >
+                 value={id}
+                label="collecteur"
+                onChange={(e) => setid(e.target.value)}
+                >
                 {Collecteur?.map((list) => 
                     <MenuItem value={list.id}>{list.utilisateur.nom + " " + list.utilisateur.prenom}</MenuItem>
-                
+
                 )}
-                
-                
-            </Select>
+                </Select>
         </FormControl>
+        <button onClick={validateFilter}> Valider</button>
 
-                
-
-            </div>
-            
+              
+     </div>
+     {InformationEtape?.map((list, _index) =>
+{return(
+    
+    
+ <ListItem className="listItemBody">
+                <ListItemText className='listItem' primary={list.client.nomCommercial}/>
+                <ListItemText className='listItem' primary={list.client.adresse}/>
+                <ListItemText className='listItem' primary={list.client.nom}/>
+                <ListItemText className='listItem'primary={moment(list.date).format('DD.MM.YYYY à HH [h] mm')}/>
+                <ListItemText className='listItem' primary={list.client.prenom}/>
+            </ListItem>
+                    )})}
+        </div>
         
-    </div>
     )
 }
 
 export default DashboardHistoryCollecteur
+
+
+
+
+
